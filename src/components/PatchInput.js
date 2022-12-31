d640e27537a675647b244664c2f22751efef290f
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2022-12-31 14:04:33 +0100

    Adding MUI icons dependency

--- src/components/PatchInput.js
+++ src/components/PatchInput.js
@@ -1,44 +1,57 @@
 import { useState, useEffect } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
 import heroesAPI from '../services/heroesAPI';
 import '../assets/PatchInput.css';
 
 const PatchInput = ({ fieldName, propertyName, entry, setEntry }) => {
     const [newValue, setNewValue] = useState('');
+    const [toggleEdit, setToggleEdit] = useState(false);
     let originalValue = entry[propertyName];
 
     useEffect(() => {
         setNewValue(entry[propertyName]);
-    },[entry]);
+    }, [entry]);
 
     const handleOkClick = async () => {
         if (newValue.length < 3) {
             alert('New value must be a string of at least 3 characters');
-            return ;
+            return;
         }
-        const patchedEntry = await heroesAPI.updateSingleHero(entry.id, {[propertyName]: newValue});
+        const patchedEntry = await heroesAPI.updateSingleHero(entry.id, { [propertyName]: newValue });
         if (patchedEntry.id) {
             setEntry(patchedEntry);
             originalValue = patchedEntry[propertyName];
+            setToggleEdit(!toggleEdit);
         }
         else {
             alert('Unable to patch the hero');
-        } 
+        }
     }
 
-    const handleCancelClick = () => setNewValue(originalValue);
+    const handleCancelClick = () => {
+        setNewValue(originalValue);
+        setToggleEdit(!toggleEdit);
+    }
 
     return (
-        <div className='patch-input-container'>
-            <label className='patch-input-label'>
-                {`${fieldName}:`}
-                <input className='patch-input-input'
-                    value={newValue}
-                    onChange={e => setNewValue(e.target.value)}
-                >
-                </input>
-            </label>
-            <button onClick={handleOkClick}>OK</button>
-            <button onClick={handleCancelClick}>Cancel</button>
+        <div className='patch-input'>
+            <label className='patch-input-label'>{`${fieldName}:`}</label>
+            <input className={`patch-input-input ${toggleEdit && 'patch-input-input_enabled'}`}
+                value={newValue}
+                onChange={e => setNewValue(e.target.value)}
+                disabled={toggleEdit ? false : true}
+            >
+            </input>
+            <div className='patch-input-btns'>
+            {toggleEdit ?
+                <>
+                    <button className='patch-input-ok' onClick={handleOkClick}>OK</button>
+                    <button className='patch-input-cancel' onClick={handleCancelClick}>Cancel</button>
+                </>
+                :
+                    <EditIcon fontSize='small' onClick={() => setToggleEdit(!toggleEdit)} />
+                }
+            </div>
         </div>
     );
 }
