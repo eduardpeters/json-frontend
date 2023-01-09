51bd063f0b33f9eadb6526f27d3398ca76767c49
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-01-09 08:50:32 +0100

    Setting up react-router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App.js';
+import ErrorPage from './components/ErrorPage.js';
+import './assets/index.css';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
